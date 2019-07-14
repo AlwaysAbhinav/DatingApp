81fb551e8c9e451f3fdc8ad505e354fb969508e2
Abhinav <abhinav.shrivastava@gmail.com>
2019-07-14 15:13:55 +0530

    added reactive form

--- DatingApp.API/DTOs/UserForRegisterDTO.cs
+++ DatingApp.API/DTOs/UserForRegisterDTO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.DTOs
@@ -10,5 +11,24 @@ namespace DatingApp.API.DTOs
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "Password should be between 4 and 8 characters")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastAcive { get; set; }
+
+        public UserForRegisterDTO()
+        {
+            Created = DateTime.Now;
+            LastAcive = DateTime.Now;
+        }
     }
 }
